bf8be6ed385ec5fc79a8fd9b117c538e810175b5
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-19 08:06:22 -0400

    Updated from Brython Server: 3/19/2018 8:06:20 AM

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#3/19/18
+#functionDemo.py
+
+def double(thingToDouble):
+    print(thingToDouble * 2)
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+        
 
