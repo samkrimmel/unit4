68e16f1797575dcb0cd572da5f795a942b7e7716
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-19 12:13:40 -0400

    Updated from Brython Server: 3/19/2018 12:13:38 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -2,6 +2,20 @@
 #3/19/18
 #colorChangeWindow.py - pops up window that changes to a random color every time you click it
 
+num = randint(1,7)
+    if num == 1:
+        color = white
+    elif num ==2:
+        color = black
+    elif num ==3:
+        color = yellow
+    elif num == 4:
+        color = blue
+    elif num == 5:
+        color = red
+    else:
+        color = green
+
 white = Color(0xffffff,1)
 black = Color(0x000000,1)
 yellow = Color(0xffe500,1)
@@ -10,4 +24,4 @@ red = Color(0xff0f0f,1)
 green = Color(0x08d63f,1)
 
 def mouseClick(event)
-    RectangleAsset(300,60, black)
+    RectangleAsset(300,60, color)
