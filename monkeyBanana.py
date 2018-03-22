cbf5cc85501c28ae5cb0368c6991a1aa3c373a24
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-22 08:44:36 -0400

    Updated from Brython Server: 3/22/2018 8:44:32 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -15,30 +15,42 @@ def moveRight(event):
         monkey.x += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
+            data['score'] += 50
+            print(data['score'])
 
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
+            data['score'] += 50
+            print(data['score'])
 
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
+            data['score'] += 50
+            print(data['score'])
 
 def moveLeft(event):
     if monkey.x > 0 :
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
+            data['score'] += 50
+            print(data['score'])
 
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
     
 if __name__ == '__main__':
+    
+    #hold variables in a dictionary
+    data = {}
+    data['score'] = 0
     
     #colors
     green = Color(0x006600,1)
