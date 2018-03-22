5fc558a8e26c473f0129b7877084c9db12bc4361
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-22 08:12:48 -0400

    Updated from Brython Server: 3/22/2018 8:12:44 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     
     #colors
     green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
     
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
     
