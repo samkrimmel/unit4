03a13c8618307c00ce9af0daf80b17750b919215
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-22 08:30:33 -0400

    Updated from Brython Server: 3/22/2018 8:30:29 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -11,6 +11,8 @@ CELL_SIZE = 20
 
 def moveRight(event):
     monkey.x += CELL_SIZE
+    if monkey.x == banana.x and monkey.y == banana.y:
+        print('YEEET')
 
 def moveUp(event):
     monkey.y -= CELL_SIZE
