facea32e73f368f44144d38e4e88d2121dd12de7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-22 08:07:46 -0400

    Updated from Brython Server: 3/22/2018 8:07:42 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -1,1 +1,16 @@
+#Sam Krimmel
+#3/22/18
+#monkeyBanana.py - the best game ever
 
+from ggame import *
+
+if __name__ == '__main__':
+    
+    #colors
+    green = Color(0x006600,1)
+    
+    jungleBox = RectagleAsset(800,500,LineStyle(1,green),green)
+    
+    sprite(JungleBox)
+    
+    App().run()
