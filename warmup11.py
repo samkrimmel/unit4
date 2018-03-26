f1c72d77cea02edc381f6b04ff5f974fcdeee4a2
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-26 08:27:59 -0400

    Updated from Brython Server: 3/26/2018 8:27:59 AM

--- warmup11.py
+++ warmup11.py
@@ -3,10 +3,9 @@
 #warmup11.py - write a function that returns True if a number is prime and false otherwise
 
 def prime(x):
-    divisible = 0
     for i in range(2,x):
         if x%i==0:
-            divisible += 1
-    return divisible==0
+            return False
+    return True
     
 print(prime(1))
