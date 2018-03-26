743b7993021a597f28609e2fb48e12b164ae9b7a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-26 08:07:31 -0400

    Updated from Brython Server: 3/26/2018 8:07:31 AM

--- localDemo.py
+++ localDemo.py
@@ -2,9 +2,11 @@
 #3/26/18
 #localDemo.py - what is a local variables?
 
-def f(x):
-    x = 20
-
+def f():
+    x = 20 #x within the function exists only within the function
+    y = 200
+    
 x = 2
-f(x)
+f()
 print(x)
+print(y)
