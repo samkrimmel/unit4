0160981bbd33c1e043857e02dfdd011898344fb0
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-26 08:33:26 -0400

    Updated from Brython Server: 3/26/2018 8:33:26 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -11,20 +11,19 @@ blue = Color(0x4da9f9,1)
 red = Color(0xff0f0f,1)
 green = Color(0x08d63f,1)
 
-num = randint(1,7)
-if num == 1:
-    color = white
-elif num ==2:
-    color = black
-elif num ==3:
-    color = yellow
-elif num == 4:
-    color = blue
-elif num == 5:
-    color = red
-else:
-    color = green
-
 def mouseClick(event):
-    window = RectangleAsset(300,60, color)
+    num = randint(1,7)
+    if num == 1:
+        color = white
+    elif num ==2:
+        color = black
+    elif num ==3:
+        color = yellow
+    elif num == 4:
+        color = blue
+    elif num == 5:
+        color = red
+    else:
+        color = green
+    window = RectangleAsset(300,60,color)
     Sprite(window)
