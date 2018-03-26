e12ca83474bf476c1679854dd216033b1a493e5d
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-26 08:42:08 -0400

    Updated from Brython Server: 3/26/2018 8:42:08 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -13,18 +13,18 @@ red = Color(0xFF0F0F,1)
 green = Color(0x08D63F,1)
 
 num = randint(1,7)
-    if num == 1:
-        color = white
-    elif num ==2:
-        color = black
-    elif num ==3:
-        color = yellow
-    elif num == 4:
-        color = blue
-    elif num == 5:
-        color = red
-    else:
-        color = green
+if num == 1:
+    color = white
+elif num ==2:
+    color = black
+elif num ==3:
+    color = yellow
+elif num == 4:
+    color = blue
+elif num == 5:
+    color = red
+else:
+    color = green
         
 def mouseClick(event):
     num = randint(1,7)
