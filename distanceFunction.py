5fce9b05ba16b4a6de7b6b2be6bb8928fde85534
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-28 08:29:43 -0400

    Updated from Brython Server: 3/28/2018 8:29:41 AM

--- distanceFunction.py
+++ distanceFunction.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#3/28/18
+#distanceFunction.py - makes the distance formula a function
 
+from math import sqrt
+
+def distance(X1,Y1,X2,Y2):
+    return sqrt(((X2-X1)**2)+((Y2-Y1)**2)))
+
+print(distance(3,4,-5,2))
