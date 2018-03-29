37800df517f02cd220d31978f07d27c8a947cb2c
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-29 08:49:53 -0400

    Updated from Brython Server: 3/29/2018 8:49:51 AM

--- triangle.py
+++ triangle.py
@@ -2,7 +2,15 @@
 #3/29/18
 #triangle.py - computes area of triangle given vertex coordinates
 
+from math import sqrt
+
 def triangle(x1,y1,x2,y2,x3,y3):
     side12 = sqrt(((x2-x1)**2)+((y2-y1)**2))
     side23 = sqrt(((x3-x2)**2)+((y3-y2)**2))
     side13 = sqrt(((x3-x1)**2)+((y3-y1)**2))
+    s = 0.5*(side12 + side23 + side13)
+    return sqrt(s((s-side12)(s-side23)(s-side13)))
+    
+print(triangle(3,4,-5,2,-7,1))
+
+    
