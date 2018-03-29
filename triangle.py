b937531ea39896fbff64a98ea1d7fd512ce4bf36
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-29 08:50:08 -0400

    Updated from Brython Server: 3/29/2018 8:50:06 AM

--- triangle.py
+++ triangle.py
@@ -9,7 +9,7 @@ def triangle(x1,y1,x2,y2,x3,y3):
     side23 = sqrt(((x3-x2)**2)+((y3-y2)**2))
     side13 = sqrt(((x3-x1)**2)+((y3-y1)**2))
     s = 0.5*(side12 + side23 + side13)
-    return sqrt(s((s-side12)(s-side23)(s-side13)))
+    return float(sqrt(s((s-side12)(s-side23)(s-side13))))
     
 print(triangle(3,4,-5,2,-7,1))
 
