a90821b8ac2415fcf9f6af659f1777ea040ff719
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-29 08:38:08 -0400

    Updated from Brython Server: 3/29/2018 8:38:05 AM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#3/29/18
+#stringIntersect.py - makes string of letters that appear in both words
 
+def stringIntersect(word1,word2):
+    combword = word1 + word2
+    final = ' '
+    for ch in combword:
+        if ch.lower() not in final.lower():
+            final += ch.lower()
