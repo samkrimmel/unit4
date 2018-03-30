b12b8d7b417c2014e198e024cfd89e4e9445aa7b
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-30 08:38:16 -0400

    Updated from Brython Server: 3/30/2018 8:38:15 AM

--- triangle.py
+++ triangle.py
@@ -18,7 +18,7 @@ def triangle(x1,y1,x2,y2,x3,y3):
     side1 = distance(x1,y1,x2,y2)
     side2 = distance(x1,y1,x3,y3)
     side3 = distance(x2,y2,x3,y3)
-    s = 0.5*(side1 + side2 + side3)
+    s = (side1 + side2 + side3)/2
     return sqrt(s*((s-side1)*(s-side2)*(s-side3)))
 
 print(triangle(x1,y1,x2,y2,x3,y3))
