556389c2a02e0bc1b3a2b8a3ab14e5c3b6befba6
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-02 08:16:34 -0400

    Updated from Brython Server: 4/2/2018 8:16:34 AM

--- quiz4.py
+++ quiz4.py
@@ -2,6 +2,19 @@
 #4/2/18
 #quiz4.py
 
-def count(a):
-    for i in range(1,a+1):
+def count(num):
+    for i in range(1,num+1):
         print(i)
+
+def excitedPrint(word):
+    print(word.upper(),'!!!')
+
+def firstLetter(word):
+    for ch in word:
+        return ch
+
+def repeats(a,b,c):
+    if a==b or a==c:
+        return True
+    else:
+        return False
